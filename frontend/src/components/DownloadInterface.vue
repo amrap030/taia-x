f0ed00d5fb33ac4d2c42a0e0db6a160ac35445a3
Elio Demollari <eliodemollari@MAC2021HJ109.local>
2022-01-09 17:04:15 +0100

    Add download interface component

---
+++ frontend/src/components/DownloadInterface.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label>Select the format of the dataset:</label>
+    <br />
+    <select v-model="selected">
+      <option>XLSX</option>
+      <option>CSV</option>
+      <option>JSON</option>
+    </select>
+    <br />
+    <button type="submit">Download</button>
+  </form>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    onSubmit() {
+      console.log('Label value: ',);
+    }
+  },
+  data() {
+    return {
+      selected: ""
+    };
+  }
+};
+</script>
